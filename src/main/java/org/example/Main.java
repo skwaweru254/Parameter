3b66155db72ba9a76905aaea48808d83235d5bbb
Simon <simon@mspace.co.ke>
2023-06-12 17:11:50 +0300

    Initial commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,5 +6,13 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println(args[0]);
+
+        Scanner ready = new Scanner(System.in);
+
+        int a = ready.nextInt();
+        String name = ready.nextLine();
+
+        System.out.println(a + name);
+
     }
 }
