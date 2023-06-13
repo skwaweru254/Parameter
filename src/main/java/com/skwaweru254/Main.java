c0bf48097c70a889d5bbbc2f35eb06b8cea77a5d
Simon <simon@mspace.co.ke>
2023-06-13 12:25:01 +0300

    ...

--- src/main/java/com/skwaweru254/Main.java
+++ src/main/java/com/skwaweru254/Main.java
@@ -1,14 +1,11 @@
 package com.skwaweru254;
 
-import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) {
 
+        Primitive obj = new Primitive();
 
-        Scanner ready = new Scanner(System.in);
-
-        System.out.println(ready.nextInt());
+        System.out.println(obj);
 
 
     }
--- src/main/java/com/skwaweru254/Primitive.java
+++ src/main/java/com/skwaweru254/Primitive.java
@@ -1,9 +1,17 @@
 package com.skwaweru254;
 
+import com.sun.xml.internal.ws.api.model.wsdl.WSDLOutput;
+
 public class Primitive {
 
+
     int a = 10;
-    String name = "Simon";
+    char letter = 's';
+    float marks = 92.12f;
+    double largeDecimalNumbers = 45454545.455;
+    long largeInteger = 787868931122332l;
+    boolean check = false;
+
 
 
 }
