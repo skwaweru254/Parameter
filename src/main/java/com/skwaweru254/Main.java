1e7584f768bd6b27c771effbf5134ac7c5665fae
Simon <simon@mspace.co.ke>
2023-06-13 11:04:37 +0300

    ...

---
+++ src/main/java/com/skwaweru254/Main.java
@@ -0,0 +1,15 @@
+package com.skwaweru254;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+
+        Scanner ready = new Scanner(System.in);
+
+        System.out.println(ready.nextInt());
+
+
+    }
+}
---
+++ src/main/java/com/skwaweru254/Primitive.java
@@ -0,0 +1,9 @@
+package com.skwaweru254;
+
+public class Primitive {
+
+    int a = 10;
+    String name = "Simon";
+
+
+}
