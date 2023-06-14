ce34a9abba0a2bc3d94ab3d74d3e743e8bb2b1c1
Simon <simon@mspace.co.ke>
2023-06-14 12:25:18 +0300

    ...

--- src/main/java/com/skwaweru254/Main.java
+++ src/main/java/com/skwaweru254/Main.java
@@ -1,7 +1,11 @@
 package com.skwaweru254;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+
+
 
         Primitive obj = new Primitive();
 
@@ -9,7 +13,20 @@ public class Main {
 
         int a = 8_000_000;
 
-        System.out.println("Underscore are ignored:"+a);
+        System.out.println("Underscore are ignored, a="+a);
+
+        //user input
+        Scanner input = new Scanner(System.in);
+
+        String name = input.nextLine();
+
+        System.out.println("Input from the previous line :" +name);
+
+        String b = input.next();
+
+        System.out.println("Input the previous first word :" +b);
+
+
 
 
 
